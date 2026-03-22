99d64f00acbe4221aaf1855f3871febbe4b98435
s10pao <99454468+s10pao@users.noreply.github.com>
2026-03-21 20:52:45 -0400

    Create custom_config.h

---
+++ miryoku/custom_config.h
@@ -0,0 +1,8 @@
+// Copyright 2021 Manna Harbour
+// https://github.com/manna-harbour/miryoku
+
+#define MIRYOKU_KLUDGE_GLOBALSHIFTFUNCTIONS
+
+#define MIRYOKU_KLUDGE_SOFT_OFF
+
+#define MIRYOKU_KLUDGE_DOUBLETAPBOOT
