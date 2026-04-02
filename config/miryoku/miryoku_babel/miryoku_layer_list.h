ca6ce135768ffe7f1abf2953a6a1eed001465fb3
s10pao <99454468+s10pao@users.noreply.github.com>
2026-04-02 19:46:27 -0400

    Removed s10pao in layer names

--- config/miryoku/miryoku_babel/miryoku_layer_list.h
+++ config/miryoku/miryoku_babel/miryoku_layer_list.h
@@ -7,16 +7,16 @@
 #if !defined (MIRYOKU_LAYER_LIST)
 
 #define MIRYOKU_LAYER_LIST \
-MIRYOKU_X(BASE,   "Base   S10PAO") \
-MIRYOKU_X(EXTRA,  "Extra  S10PAO") \
-MIRYOKU_X(TAP,    "Tap    S10PAO") \
-MIRYOKU_X(BUTTON, "Button S10PAO") \
-MIRYOKU_X(NAV,    "Nav    S10PAO") \
-MIRYOKU_X(MOUSE,  "Mouse  S10PAO") \
-MIRYOKU_X(MEDIA,  "Media  S10PAO") \
-MIRYOKU_X(NUM,    "Num    S10PAO") \
-MIRYOKU_X(SYM,    "Sym    S10PAO") \
-MIRYOKU_X(FUN,    "Fun    S10PAO")
+MIRYOKU_X(BASE,   "Base") \
+MIRYOKU_X(EXTRA,  "Extra") \
+MIRYOKU_X(TAP,    "Tap") \
+MIRYOKU_X(BUTTON, "Button") \
+MIRYOKU_X(NAV,    "Nav") \
+MIRYOKU_X(MOUSE,  "Mouse") \
+MIRYOKU_X(MEDIA,  "Media") \
+MIRYOKU_X(NUM,    "Num") \
+MIRYOKU_X(SYM,    "Sym") \
+MIRYOKU_X(FUN,    "Fun")
 
 #if 1
 #define U_BASE   0
